e62ad0edcc0086046c763476d483e1c7a93e69e6
antediluvian <vladamitke@gmail.com>
2019-11-24 23:35:30 +0100

    First js commit

---
+++ js/variables_&_operators.js
@@ -0,0 +1,35 @@
+// 1. Zadatak
+
+var Peter,
+    Jim,
+
+Peter = "3500$";
+Jim = "5000$";
+
+console.log(Peter + " is less than " + Jim);
+console.log(Peter >= Jim);
+
+
+// 2. Zadatak
+
+var John,
+    Mike,
+    a,
+
+John = 38;
+a =8;
+Mike = 2;
+
+console.log("Mike is " + (John + a) / Mike + " years old");  
+
+// 3.Zadatak
+
+var firstFlock,
+    secondFlock,
+
+firstFlock = 78;
+secondFlock = 57;
+
+console.log(firstFlock - 12);
+console.log(secondFlock + 12 - 4); 	
+console.log((firstFlock - 12) >= (secondFlock + 12 - 4));    
